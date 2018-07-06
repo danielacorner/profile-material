ff09cd753d200d1d9e68a579d24d9c892ebb00f5
Daniel Corner <dcorn068@gmail.com>
2018-07-06 16:23:18 -0400

    Activate navbar tabs by active route

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,27 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   template: `
-  
-  <div class="content row grey darken-3 white-text left">
-  <div class="container">
-    <div class="row fadeUp">
-      <div class="col s8 m6">
-        <h3 class="profile-title center">Hello world!</h3>
-        <p>
-          I'm a Junior Web Developer looking to get started in the industry. I come from a background in Engineering and Biotechnology.
-        </p>
-        <p>
-        </p>
-        <ul>
-          <li>I came to the web to <a href>play</a> with <a href>data</a></li>
-          <li>Some of my best friends are <a href>spreadsheets</a></li>
-          <li><a href>link3</a></li>
-        </ul>
+
+  <div class="content row white-text left">
+    <div class="container">
+      <div class="row fadeUp">
+        <div class="col s8 m6">
+          <h3 class="profile-title center">Hello world!</h3>
+          <p>
+            I'm a Junior Web Developer looking to get started in the industry. I come from a background in Engineering and Biotechnology.
+          </p>
+          <p>
+          </p>
+          <ul>
+            <li>I came to the web to <a href>play</a> with <a href>data</a></li>
+            <li>Some of my best friends are <a href>spreadsheets</a></li>
+            <li><a href>link3</a></li>
+          </ul>
+        </div>
+        <div class="img-container col s4 m5 offset-m1"><img class="responsive-img right circle z-depth-3"
+        src="./assets/headshot.jpg" alt="headshot"></div>
       </div>
-      <div class="img-container col s4 m5 offset-m1"><img class="responsive-img right circle z-depth-3" 
-      src="./assets/headshot.jpg" alt="headshot"></div>
     </div>
-  </div>
   </div>
   `,
   styles: [`
@@ -46,6 +46,8 @@ import { Component, OnInit } from '@angular/core';
   .img-container {
     margin: 10vh 0 0 0;
   }
+
+
   @keyframes fadeUpAnimation{
     0%{
         transform: rotate(0) scale(0.9) translateY(5%);
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-projects',
+  selector: "app-projects",
   template: `
-  <mat-grid-list cols="2" rowHeight="550px">
-  
+  <div class="container">
+  <mat-grid-list cols="2" rowHeight="500px" gutterSize="24px">
+
       <mat-grid-tile *ngFor="let project of projects">
         <!-- {{project.title | uppercase}} -->
-        <mat-card (click)="showModal(project.id)" class="project-card">
+        <mat-card (click)="showModal(project.id)" class="project-card z-depth-3">
           <mat-card-header>
             <div mat-card-avatar [ngStyle]="{'background-image':'url(' + project.cardImageUrl + ')'}" class="project-header-image"></div>
             <mat-card-title>{{project.title}}</mat-card-title>
@@ -29,28 +30,33 @@ import { Component, OnInit } from '@angular/core';
       </mat-grid-tile>
 
   </mat-grid-list>
+  </div>
 
   <app-modal *ngIf="modalOn"></app-modal>
   `,
-  styles: [`
-  
-  .card-image {
-    height: 320px;
-  }
-  .project-card {
-    max-width: 400px;
-  }
-  .project-header-image {
-    background-size: cover;
-  }
-`]
+  styles: [
+    ` .container{
+        width: 95%;
+        margin-top: 25px;
+      }
+      .card-image {
+        height: 320px;
+      }
+      .project-card {
+        max-width: 500px;
+      }
+      .project-header-image {
+        background-size: cover;
+      }
+    `
+  ]
 })
 export class ProjectsComponent implements OnInit {
-  public site = { url: "https://dcorn068.github.io/profile-material/"}
+  public site = { url: "https://dcorn068.github.io/profile-material/" };
   public modalOn = false;
   public projects = [
     {
-      id: "1", 
+      id: "1",
       title: "Pave: The Canadian Job Explorer (D3.js)",
       subtitle: "interactive data visualization",
       cardLogoUrl: "./assets/pave_d3.bmp",
@@ -58,10 +64,10 @@ export class ProjectsComponent implements OnInit {
       cardLogoAltText: "Logo for Pave (D3.js)",
       cardImageUrl: "./assets/pave_d3_logo.png",
       description: "Pave is an interactive data visualization... ",
-      siteLink: "dcorn068.github.io/Pave-D3/",
+      siteLink: "dcorn068.github.io/Pave-D3/"
     },
     {
-      id: "2", 
+      id: "2",
       title: "a-h-l.net",
       subtitle: "responsive portfolio site",
       cardLogoUrl: "./assets/ahl.bmp",
@@ -69,10 +75,10 @@ export class ProjectsComponent implements OnInit {
       cardLogoAltText: "Logo for a-h-l.net",
       cardImageUrl: "./assets/ahl.bmp",
       description: "A basic portfolio website... ",
-      siteLink: "dcorn068.github.io/a-h-l/index.html",
+      siteLink: "dcorn068.github.io/a-h-l/index.html"
     },
     {
-      id: "3", 
+      id: "3",
       title: "Pave: The Canadian Job Explorer (Tableau)",
       subtitle: "interactive data visualization",
       cardLogoUrl: "./assets/pave_tableau.bmp",
@@ -80,14 +86,12 @@ export class ProjectsComponent implements OnInit {
       cardLogoAltText: "Logo for Pave (Tableau)",
       cardImageUrl: "./assets/pave_tableau_logo.bmp",
       description: "Pave is an interactive data visualization... ",
-      siteLink: "public.tableau.com/profile/daniel.corner#!/vizhome/PavePrototype4_1/Welcome",
-    },
-    
-  ]
+      siteLink:
+        "public.tableau.com/profile/daniel.corner#!/vizhome/PavePrototype4_1/Welcome"
+    }
+  ];
 
-  constructor() {
-
-  }
+  constructor() {}
 
   ngOnInit() {
     // this.projectlist
